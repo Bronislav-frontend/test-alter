ce675de2a50f499feefd15ae44cff84d7faeddde
Bronislav-frontend <marine4bro@gmail.com>
2023-02-25 16:54:18 +0200

    fixed loader + git-pages

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,6 @@ import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { store, persistor } from './redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
-import 'modern-normalize/modern-normalize.css';
 
 import './utils/i18n';
 
